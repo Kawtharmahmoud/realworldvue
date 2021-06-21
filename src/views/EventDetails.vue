2729ee2fe41357e0f85a99c60f0981bf6c4131d4
kawtharklayshe <kawthar.klayshe177@gmail.com>
2021-06-21 20:55:49 +0300

    hi

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,26 @@
+<template>
+<div v-if="event">
+    <h2>{{event.title}}</h2>
+    <span>{{event.time}} on {{event.date}}  @ {{event.location}}</span>
+    <p>{{event.description}}</p>
+    </div>
+</template>
+<script>
+import  EventServices  from '../services/EventServices.js';
+
+export default {
+  
+  props:['id'],
+  data(){
+      return {
+          event :null
+      }
+  },
+  created(){
+      EventServices.getEvent(this.id).then(response=>this.event= response.data)
+      .catch(error =>{
+          console.log(error)
+      })
+  }
+};
+</script>
